dca782e2a299df99514a1300aaa989c393782c82
a-tortevois <alexandre.tortevois@gmail.com>
2023-04-10 17:07:25 +0200

    Fix file format

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const { readdirSync, readFileSync, existsSync } = require("node:fs");
-const path = require("node:path");
+const {readdirSync, readFileSync, existsSync} = require('node:fs');
+const path = require('node:path');
 
 // const {fileURLToPath} = require('node:url');
 // const __filename = fileURLToPath(import.meta.url);
@@ -13,85 +13,80 @@ const inputTestFiles = [];
 const outputTestFiles = [];
 let testInputs;
 let testOutputs;
-const solution = existsSync(path.join(__dirname, "solution.js")) ? readFileSync(path.join(__dirname, "solution.js")).toString() : console.error(`Unable to open the file: solutions.js`);
+const solution = existsSync(path.join(__dirname, 'solution.js')) ? readFileSync(path.join(__dirname, 'solution.js')).toString() : console.error(`Unable to open the file: solutions.js`);
 const exit = {
-  success: true,
-  find: "",
-  expected: "",
+    success: true,
+    find: '',
+    expected: '',
 };
 
 // -- Functions definition --
 const getFilesListFromDir = (dirent) => {
-  return readdirSync(path.join(__dirname, dirent), { withFileTypes: true })
-    .filter((dirent) => dirent.isFile())
-    .map((dirent) => dirent.name);
+    return readdirSync(path.join(__dirname, dirent), {withFileTypes: true}).filter((dirent) => dirent.isFile()).map((dirent) => dirent.name);
 };
 
 const readline = () => {
-  return testInputs.shift();
+    return testInputs.shift();
 };
 
 console.log = (...data) => {
-  const expected = testOutputs.shift();
-  const find = data.join(" ");
-  if (exit.success && find !== expected) {
-    exit.success = false;
-    exit.find = find;
-    exit.expected = expected;
-  }
-  defaultConsoleLog.apply(this, data);
+    const expected = testOutputs.shift();
+    const find = data.join(' ');
+    if (exit.success && find !== expected) {
+        exit.success = false;
+        exit.find = find;
+        exit.expected = expected;
+    }
+    defaultConsoleLog.apply(this, data);
 };
 
 console.error = (...data) => {
-  if (data.length === 1) {
-    data[0] = `\x1b[31m${data}\x1b[0m`;
-  } else {
-    data.unshift("\x1b[31m");
-    data.push("\x1b[0m");
-  }
-  defaultConsoleError.apply(this, data);
-  process.exit(1);
+    if (data.length === 1) {
+        data[0] = `\x1b[31m${data}\x1b[0m`;
+    } else {
+        data.unshift('\x1b[31m');
+        data.push('\x1b[0m');
+    }
+    defaultConsoleError.apply(this, data);
+    process.exit(1);
 };
 
 // -- Main --
 const main = () => {
-  if (args[0] === "all") {
-    inputTestFiles.push(...getFilesListFromDir("inputs"));
-    outputTestFiles.push(...getFilesListFromDir("outputs"));
-  } else {
-    for (let arg of args) {
-      const testFile = `${arg}.txt`;
-      if (!existsSync(path.join(__dirname, "inputs", testFile))) {
-        console.error(`Unable to open the file: ./inputs/${testFile}`);
-      }
-      if (!existsSync(path.join(__dirname, "outputs", testFile))) {
-        console.error(`Unable to open the file: ./outputs/${testFile}`);
-      }
-      inputTestFiles.push(testFile);
-      outputTestFiles.push(testFile);
+    if (args[0] === 'all') {
+        inputTestFiles.push(...getFilesListFromDir('inputs'));
+        outputTestFiles.push(...getFilesListFromDir('outputs'));
+    } else {
+        for (const arg of args) {
+            const testFile = `${arg}.txt`;
+            if (!existsSync(path.join(__dirname, 'inputs', testFile))) {
+                console.error(`Unable to open the file: ./inputs/${testFile}`);
+            }
+            if (!existsSync(path.join(__dirname, 'outputs', testFile))) {
+                console.error(`Unable to open the file: ./outputs/${testFile}`);
+            }
+            inputTestFiles.push(testFile);
+            outputTestFiles.push(testFile);
+        }
     }
-  }
 
-  for (const testFile of inputTestFiles) {
-    if (outputTestFiles.includes(testFile)) {
-      console.warn(`-----------------------------------------------------------------------------------------------------`);
-      console.warn(`\x1b[47m\x1b[30mExec test #${testFile.split(".")[0]}\x1b[0m\n`);
-      testInputs = readFileSync(path.join(__dirname, "inputs", testFile))
-        .toString()
-        .split("\n")
-        .map((v) => v.trim());
-      testOutputs = readFileSync(path.join(__dirname, "outputs", testFile))
-        .toString()
-        .split("\n")
-        .map((v) => v.trim());
-      eval(solution);
-      if (exit.success) {
-        console.warn(`\n\x1b[32mSuccess\x1b[0m`);
-      } else {
-        console.error(`Échec\nTrouvé: ${exit.find}\nAttendu: ${exit.expected}`);
-      }
+    for (const testFile of inputTestFiles) {
+        if (outputTestFiles.includes(testFile)) {
+            testInputs = readFileSync(path.join(__dirname, 'inputs', testFile)).toString().split('\n').map((v) => v.trim());
+            testOutputs = readFileSync(path.join(__dirname, 'outputs', testFile)).toString().split('\n').map((v) => v.trim());
+            if (testInputs.length === 0 || testOutputs.length === 0) {
+                continue;
+            }
+            console.warn(`-----------------------------------------------------------------------------------------------------`);
+            console.warn(`\x1b[47m\x1b[30mExec test #${testFile.split('.')[0]}\x1b[0m\n`);
+            eval(solution);
+            if (exit.success) {
+                console.warn(`\n\x1b[32mSuccess\x1b[0m`);
+            } else {
+                console.error(`Échec\nTrouvé: ${exit.find}\nAttendu: ${exit.expected}`);
+            }
+        }
     }
-  }
 };
 
 main();
--- solution.js
+++ solution.js
@@ -2,7 +2,7 @@
 // 1. Copy/Paste the input in the ./inputs/test_X.txt file
 // 2. Copy/Paste the expected output in the./outputs/test_X.txt file
 // 3. Write your code in the solution.js file
-// 4. Exec with the command: nmp run test -- all || nmp run test -- <test_number>
+// 4. Exec with the command: npm run test -- all || npm run test -- <test_number>
 
 // Get inputs
 input = readline();
